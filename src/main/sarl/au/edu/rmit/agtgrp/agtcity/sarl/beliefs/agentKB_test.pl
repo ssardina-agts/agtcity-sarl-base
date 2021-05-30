9e49c6bfda194d57b645d92ad5f9bf53e2234bc2
ssardina <ssardina@gmail.com>
2021-05-30 19:53:37 +1000

    Added a large percept example file

--- src/main/sarl/au/edu/rmit/agtgrp/agtcity/sarl/beliefs/agentKB_test.pl
+++ src/main/sarl/au/edu/rmit/agtgrp/agtcity/sarl/beliefs/agentKB_test.pl
@@ -1,7 +1,9 @@
+:- multifile percept_sensed/3.
+
 %%%%
 %% Load this file to have the agent KB + some example percepts
 %% Useful for development and testing
 :- include(agentKB).
 
-:- include(percepts_examples).  % Add example percepts
+:- include(percepts_examples_39).  % Add example percepts
 
